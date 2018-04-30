2b28676d76508a4768540d36c5dd176acdf7f5fc
Benjamin Perez <baperez@miuandes.cl>
2018-04-30 00:02:24 -0300

    bug fixed

--- crowdfinder/app/controllers/concerns/categories_controller.rb
+++ crowdfinder/app/controllers/concerns/categories_controller.rb
@@ -3,12 +3,13 @@ class CategoriesController < ApplicationController
     @category = Category.new
 
     respond_to do |format|
-        if @category.save
-          format.html { redirect_to @category, notice: 'Category was successfully created.' }
-          format.json { render :show, status: :created, location: @category }
-        else
-          format.html { render :new }
-          format.json { render json: @category.errors, status: :unprocessable_entity }
-        end
+      if @category.save
+        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
