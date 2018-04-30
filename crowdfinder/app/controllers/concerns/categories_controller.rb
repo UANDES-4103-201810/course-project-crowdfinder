52118144a2c67a1986d6a1b785139a9b3b993a57
Benjamin Perez <baperez@miuandes.cl>
2018-04-29 23:55:51 -0300

    categories controller added

---
+++ crowdfinder/app/controllers/concerns/categories_controller.rb
@@ -0,0 +1,14 @@
+class CategoriesController < ApplicationController
+  def create
+    @category = Category.new
+
+    respond_to do |format|
+        if @category.save
+          format.html { redirect_to @category, notice: 'Category was successfully created.' }
+          format.json { render :show, status: :created, location: @category }
+        else
+          format.html { render :new }
+          format.json { render json: @category.errors, status: :unprocessable_entity }
+        end
+  end
+end
