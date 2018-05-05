3779ae18211da8cf004d1a35a09711c78e4cc5b6
Benjamin Perez <baperez@miuandes.cl>
2018-05-05 18:34:28 -0300

    routes updated

--- crowdfinder/config/routes.rb
+++ crowdfinder/config/routes.rb
@@ -2,42 +2,47 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :projects
+  resources :funders
+  resources :categories
+  resources :merches
+  resources :promises
+  resources :wishes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/users/:id", to: "users#show"
-  post "/users/create", to: "users#create"
-  post "/users/update", to: "users#update"
-  post "/users/destroy", to: "users#destroy"
-
-  get "/projects/:id", to: "projects#show"
-  post "/projects/create", to: "projects#create"
-  post "/projects/update", to: "projects#update"
-  post "/projects/destroy", to: "projects#destroy"
-
-  get "/funders/:id", to: "funders#show"
-  post "/funders/create", to: "funders#create"
-  post "/funders/update", to: "funders#update"
-  post "/funders/destroy", to: "funders#destroy"
-
-  get "/categories/:id", to: "categories#show"
-  post "/categories/create", to: "categories#create"
-  post "/categories/update", to: "categories#update"
-  post "/categories/destroy", to: "categories#destroy"
-
-  get "/merches/:id", to: "merches#show"
-  post "/merches/create", to: "merches#create"
-  post "/merches/update", to: "merches#update"
-  post "/merches/destroy", to: "merches#destroy"
-
-  get "/promises/:id", to: "promises#show"
-  post "/promises/create", to: "promises#create"
-  post "/promises/update", to: "promises#update"
-  post "/promises/destroy", to: "promises#destroy"
-
-  get "/wishes/:id", to: "wishes#show"
-  post "/wishes/create", to: "wishes#create"
-  post "/wishes/update", to: "wishes#update"
-  post "/wishes/destroy", to: "wishes#destroy"
+  #get "/users/:id", to: "users#show"
+  #post "/users/create", to: "users#create"
+  #post "/users/update", to: "users#update"
+  #post "/users/destroy", to: "users#destroy"
+#
+  #get "/projects/:id", to: "projects#show"
+  #post "/projects/create", to: "projects#create"
+  #post "/projects/update", to: "projects#update"
+  #post "/projects/destroy", to: "projects#destroy"
+#
+  #get "/funders/:id", to: "funders#show"
+  #post "/funders/create", to: "funders#create"
+  #post "/funders/update", to: "funders#update"
+  #post "/funders/destroy", to: "funders#destroy"
+#
+  #get "/categories/:id", to: "categories#show"
+  #post "/categories/create", to: "categories#create"
+  #post "/categories/update", to: "categories#update"
+  #post "/categories/destroy", to: "categories#destroy"
+#
+  #get "/merches/:id", to: "merches#show"
+  #post "/merches/create", to: "merches#create"
+  #post "/merches/update", to: "merches#update"
+  #post "/merches/destroy", to: "merches#destroy"
+#
+  #get "/promises/:id", to: "promises#show"
+  #post "/promises/create", to: "promises#create"
+  #post "/promises/update", to: "promises#update"
+  #post "/promises/destroy", to: "promises#destroy"
+#
+  #get "/wishes/:id", to: "wishes#show"
+  #post "/wishes/create", to: "wishes#create"
+  #post "/wishes/update", to: "wishes#update"
+  #post "/wishes/destroy", to: "wishes#destroy"
 
   get "home/index"
   root "home#index"
