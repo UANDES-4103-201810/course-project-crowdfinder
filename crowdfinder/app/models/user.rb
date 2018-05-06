0fd552a701ca714d54478bd6ba50a9508df6fd4e
Benjamin Perez <baperez@miuandes.cl>
2018-05-05 22:55:55 -0300

    sing up working without name validation

--- crowdfinder/app/models/user.rb
+++ crowdfinder/app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :wishes
   has_many :projects, through: :wishes
 
-  validates :name, length: {minimum: 3}, allow_blank: false
+  #validates :name, length: {minimum: 3}, allow_blank: false
   validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
   validates :password, length: {minimum: 8}, allow_blank: false, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "Password more than 8 alphanumeric characters"}
 end
