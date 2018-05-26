df2ace87c4be5ae8f53794d742c9feeb633c9c7f
Francisco Alvarez <s2globalmail@gmail.com>
2018-05-25 23:04:09 -0400

    categories button and admin filter added

--- crowdfinder/app/controllers/categories_controller.rb
+++ crowdfinder/app/controllers/categories_controller.rb
@@ -1,5 +1,8 @@
 class CategoriesController < ApplicationController
   def index
+    unless current_user.is_admin
+      redirect_to(home_index_path)
+    end
     @categories = Category.all
   end
 
