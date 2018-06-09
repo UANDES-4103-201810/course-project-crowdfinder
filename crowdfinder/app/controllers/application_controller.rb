6f0fd87b151fb814086e9968bc9332e7d69d651a
benjamin perez <benjaperezg@gmail.com>
2018-06-08 21:58:03 -0400

    all fields in user are changeable in devise!

--- crowdfinder/app/controllers/application_controller.rb
+++ crowdfinder/app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controler?
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
  protected
  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
-    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
-    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
-    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
-    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
-    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
  end
 
 end
