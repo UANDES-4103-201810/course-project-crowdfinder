f9a66bf2258c0d51a1177db7a5d7d0ca5ade755a
benjamin perez <benjaperezg@gmail.com>
2018-06-08 21:47:41 -0400

    seems i fixes the devise problem

--- crowdfinder/app/controllers/application_controller.rb
+++ crowdfinder/app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
-  #before_action :configure_permitted_parameters, if: :devise_controler?
+  before_action :configure_permitted_parameters, if: :devise_controler?
 
- #protected
- #def configure_permitted_parameters
- #  #devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
- #  #devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
- #  #devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
- #  #devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
- #  #devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
- #  #devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
- #end
+ protected
+ def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
+    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
+    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
+    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
+    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
+    devise_parameter_sanitizer.permit(sign_up, keys: [:name, :is_admin])
+ end
+
 end
