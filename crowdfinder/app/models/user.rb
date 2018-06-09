a84ce98c60a13d755e719a0f2acab2e2c69eafea
benjamin perez <benjaperezg@gmail.com>
2018-06-08 22:25:42 -0400

    devise forms fixed

--- crowdfinder/app/models/user.rb
+++ crowdfinder/app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :projects, through: :wishes
 
   #validates :name, length: {minimum: 3}, allow_blank: false
+  validates :username, uniqueness: true
   validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
   validates :password, length: {minimum: 8}, allow_blank: false, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "Password more than 8 alphanumeric characters"}
 end
--- crowdfinder/db/schema.rb
+++ crowdfinder/db/schema.rb
@@ -58,7 +58,9 @@ ActiveRecord::Schema.define(version: 2018_05_25_233737) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
     t.boolean "is_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
