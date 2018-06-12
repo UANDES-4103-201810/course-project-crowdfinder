7f1026b769a6583cb1062c8d1c563465f7769f9c
benjamin perez <benjaperezg@gmail.com>
2018-06-12 12:51:24 -0400

    value field added to funder model

--- crowdfinder/db/migrate/20180413031303_create_funders.rb
+++ crowdfinder/db/migrate/20180413031303_create_funders.rb
@@ -3,6 +3,7 @@ class CreateFunders < ActiveRecord::Migration[5.2]
     create_table :funders do |t|
       t.integer :user_id
       t.integer :promise_id
+      t.integer :value
 
       t.timestamps
     end
